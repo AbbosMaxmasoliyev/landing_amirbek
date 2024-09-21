393d414e7c1aa32de8f224e7ed21627a1dd93ed2
unknown <AbbosMaxmasoliyev>
2024-09-21 12:35:45 +0500

    Added fixed gradient

--- src/app/component/showcase.tsx
+++ src/app/component/showcase.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Showcase = () => {
     return (
         <div id='home' className='flex xl:flex-col flex-col-reverse min-h-[85vh] overflow-hidden items-center justify-between relative'>
-            <div className="flex flex-col xl:w-10/12 w-full p-4 xl:mb-5 xl:justify-end xl:h-[70vh] items-start  bg-black z-[3] xl:z-[unset]  xl:static   xl:bottom-[unset] bottom-0">
+            <div className="flex flex-col xl:w-10/12 w-full p-4 xl:mb-5 xl:justify-end xl:h-[70vh] items-start  bg-gradient-to-t from-black via-black/85 to-transparent z-[3] xl:z-[unset]  xl:static   xl:bottom-[unset] bottom-0">
                 <h1 className='xl:z-[1] z-[3] text-[40px]  xl:text-[150px] 2xl:text-[190px]  top-[17%]  text-center font-bold xl:absolute'>I&apos;M AMIRBEK</h1>
                 <p className='z-[3] xl:w-[45%] max-w-[90%] '>I&apos;m a versatile and accomplished professional with expertise spanning multiple fields. As a producer, marketer, and developer, he has established a strong presence in various industries, demonstrating his ability to create, innovate, and lead.</p>
                 <button className='py-3 px-5 bg-yellow-500 rounded-[50px] my-5 z-[3]'>Load More</button>
